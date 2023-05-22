74e0f340dac9dc0d07781bc427529f4c50fce128
George-Olufemi <georgeoluolufemi@gmail.com>
2023-05-22 11:56:34 +0100

    added features and pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useParams, useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
 
 const fetchEvents = async () => {
@@ -18,7 +18,38 @@ const fetchEvents = async () => {
 };
 
 const EventList = () => {
-  const { data: events, isLoading, error } = useQuery('events', fetchEvents);
+  const [page, setPage] = useState(1);
+  const [petsAllowed, setPetsAllowed] = useState(false);
+
+  const { data: events, isLoading, error } = useQuery(['events', page, petsAllowed], () =>
+    fetchEvents().then((events) => {
+      // Apply pagination
+      const itemsPerPage = 3;
+      const startIndex = (page - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      const paginatedEvents = events.slice(startIndex, endIndex);
+
+      // Apply petsAllowed filter
+      const filteredEvents = petsAllowed
+        ? paginatedEvents.filter((event) => event.petsAllowed)
+        : paginatedEvents;
+
+      return filteredEvents;
+    })
+  );
+
+  const navigate = useNavigate();
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+    navigate(`?page=${newPage}`, { replace: true });
+  };
+
+  const handleFilterChange = (event) => {
+    setPetsAllowed(event.target.checked);
+    setPage(1);
+    navigate(`?page=1`, { replace: true });
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -31,7 +62,15 @@ const EventList = () => {
   return (
     <div>
       <h2>All Events</h2>
-      {events.map((event: any) => (
+      <label>
+        Pets Allowed
+        <input
+          type="checkbox"
+          checked={petsAllowed}
+          onChange={handleFilterChange}
+        />
+      </label>
+      {events.map((event) => (
         <div key={event.id}>
           <h3>
             <Link to={`/event/${event.id}`}>{event.title}</Link>
@@ -39,6 +78,20 @@ const EventList = () => {
           <p>{event.description}</p>
         </div>
       ))}
+      <div>
+        <button
+          disabled={page === 1}
+          onClick={() => handlePageChange(page - 1)}
+        >
+          Previous Page
+        </button>
+        <button
+          disabled={events.length < 3}
+          onClick={() => handlePageChange(page + 1)}
+        >
+          Next Page
+        </button>
+      </div>
     </div>
   );
 };
@@ -55,7 +108,7 @@ const EventDetail = () => {
     return <div>Error: {error.message}</div>;
   }
 
-  const event = events.find((e: any) => e.id === parseInt(eventId));
+  const event = events.find((e) => e.id === parseInt(eventId));
 
   if (!event) {
     return <div>Event not found</div>;
